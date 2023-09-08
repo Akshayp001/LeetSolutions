f5858081af739b9c559d0e3915c5caa8d2278903
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2023-09-08 12:56:33 +0530

    Time: 4 ms (29.61%) | Memory: 6.8 MB (6.44%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        string word  ="";
-        string mword;
-
-
-        for(int i=0;i<s.length();i++){
-            if(isspace(s[i])){
-                if(word!="") mword = word;
-                word="";
-                continue;
-            }else{
-                word.push_back(s[i]);
-            }
-        } 
-        if(word!="") mword = word;
-        return mword.length();                    
-        
+        istringstream my_stream(s);
+        string last;
+        while(my_stream>>s){
+            last=s;
+        }               
+        return last.length();
     }
 };
