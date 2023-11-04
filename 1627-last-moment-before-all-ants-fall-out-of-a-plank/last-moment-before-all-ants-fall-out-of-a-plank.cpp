2fa60c60e208987d45806802b2836eae5b7eca14
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2023-11-04 21:41:19 +0530

    Time: 19 ms (48.28%) | Memory: 23.3 MB (14.66%) - LeetSync

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -1,26 +1,35 @@
 class Solution {
 public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) {
-        vector<char> initp(n+1,'n');
+        int ans=0;
         for(int i: left){
-            initp[i]='l';
+            ans = max(ans,abs(i-0));
         }
         for(int i: right){
-            initp[i]='r';
+            ans = max(ans,abs(i-n));
         }
 
-        int ans=0;
+        // first thinking
+        // vector<char> initp(n+1,'n');
+        // for(int i: left){
+        //     initp[i]='l';
+        // }
+        // for(int i: right){
+        //     initp[i]='r';
+        // }
 
-        for(int i=0;i<=n;i++){
-            if(initp[i]=='n') continue;
+        // int ans=0;
 
-            if(initp[i]=='l'){
-                ans = max(ans,abs(i-0));
-            }
-            if(initp[i]=='r'){
-                ans = max(ans,abs(i-n));
-            }
-        }
+        // for(int i=0;i<=n;i++){
+        //     if(initp[i]=='n') continue;
+
+        //     if(initp[i]=='l'){
+        //         ans = max(ans,abs(i-0));
+        //     }
+        //     if(initp[i]=='r'){
+        //         ans = max(ans,abs(i-n));
+        //     }
+        // }
         return ans;
     }
 };
