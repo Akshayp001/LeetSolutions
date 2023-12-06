90275e3ecf339ef63c9c8d606a9370931b5b065b
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2023-12-06 16:22:54 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (15.79%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,40 +1,11 @@
 class Solution {
 public:
-    int totalMoney(int n) {;
-        // int j=1;
-        // int nn=n;
-        // int ans = 0;
-        // for(int i=0;i<(n/7);i++){
-        //     int l = j+7;
-        //     for(int k=j;k<l;k++){
-        //         ans+=k;
-        //         cout<<k<<endl;
-        //         nn--;  
-        //     }
-        //     j=j+1;
-        // }
-        // // cout<<j<<"o"<<endl
-        // if(n%7!=0){
-        //     cout<<"after"<<endl;
-        //     int a = n%7;
-        //     while(a--){
-        //         cout<<j<<endl;
-        //         ans+=j++;
-        //     }
-        // }
-
+    int totalMoney(int n) {
         int comp = n / 7;
         int rem = n % 7;
         int ans = 0;
-        // Add complete weeks
-        for(int i=0;i<comp;i++) ans += 28+(7*i);
-        
-        // Add incomplete days
-        int baseMoney = comp + 1;  
-        for(int i = 0; i < rem; i++) {
-            ans += baseMoney + i;
-        }
-        return ans;
-        
+        for(int i=0;i<comp;i++) ans += 28+(7*i); // Add complete weeks
+        ans+= comp*rem + (rem*(rem+1))/2; // Add incomplete days
+        return ans; 
     }
 };
