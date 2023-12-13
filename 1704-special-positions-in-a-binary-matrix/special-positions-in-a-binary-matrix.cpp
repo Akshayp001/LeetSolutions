0767171af0f03af4ab66cc047cdec690c2e3f96c
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2023-12-13 14:56:14 +0530

    Time: 363 ms (5.79%) | Memory: 752.4 MB (5.53%) - LeetSync

---
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool check(vector<vector<int>> mat,int x,int y){
+        for(int i=0;i<mat.size();i++){
+            if(i==x) continue;
+            if(mat[i][y]==1) return false;
+        }
+
+        for(int j=0;j<mat[0].size();j++){
+            if(y==j) continue;
+            if(mat[x][j]==1) return false;
+        }
+
+        return true;
+
+
+    }
+    int numSpecial(vector<vector<int>>& mat) {
+        int count=0;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]) if(check(mat,i,j)) count++;
+            }
+        }
+        return count;
+        
+    }
+};
