1eab210dab379130a9f2d484d2eb3c102e87ada8
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2023-12-28 15:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (33.44%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-    vector<int> dp(5,1);
-    int t = 5;
-    for(int i=1;i<n;i++){
-        t = 1;
-        dp[4]=1;
-        for(int j=3;j>=0;j--){
-            dp[j] = dp[j+1] + dp[j];
-            t+=dp[j];
+    // // tabulation
+    // vector<int> dp(5,1);
+    // int t = 5;
+    // for(int i=1;i<n;i++){
+    //     t = 1; dp[4]=1;
+    //     for(int j=3;j>=0;j--){
+    //         dp[j] = dp[j+1] + dp[j];
+    //         t+=dp[j];
+    //     }
+    // }
+    // return t;  
+    // space optimisation
+    int a=1, e=1, i=1, o=1, u=1;
+        while(--n){
+            o += u;
+            i += o;
+            e += i;
+            a += e;
         }
-    }
-    return t;
-        
+    return a+e+i+o+u;
     }
 };
