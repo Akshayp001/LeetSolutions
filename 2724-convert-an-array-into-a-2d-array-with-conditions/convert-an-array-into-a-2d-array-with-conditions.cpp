b30ffbc0a4ff5824baf2be6343e0ca66224c1d63
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-02 23:56:49 +0530

    Time: 15 ms (41.19%) | Memory: 29.9 MB (70.96%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,29 +1,21 @@
-struct ComparePairs {
-    bool operator()(const pair<int, int>& p1, const pair<int, int>& p2) {
-        return p1.second < p2.second;
-    }
-};
-
-
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        unordered_map<int,int> m;
-        priority_queue<pair<int,int>,vector<pair<int, int>>, ComparePairs> pq;
-
-        for(auto i:nums) m[i]++;
-
-        for(auto i:m) pq.push(i);
-
-        vector<vector<int>> ans(pq.top().second,vector<int>());
-        while(!pq.empty()){
-            int t = pq.top().first;
-            for(int i=0;i<m[t];i++){
-                ans[i].push_back(t);
+        unordered_map<int, int> m;
+        int maxi = 0;
+        for (auto i : nums) {
+            m[i]++;
+            maxi = max(maxi, m[i]);
+        };
+        vector<vector<int>> ans(maxi, vector<int>()); 
+        
+        for (auto mi : m) {
+            int freq = mi.second; 
+            int i = 0;
+            while (freq--) { 
+                ans[i++].push_back(mi.first);
             }
-            pq.pop();
         }
         return ans;
-
     }
 };
