e51efb5930293e017031bf7dbc601e876c3654f9
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-08 21:53:17 +0530

    Time: 274 ms (26.59%) | Memory: 10.8 MB (55.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -19,19 +19,33 @@ public:
     int lengthOfLIS(vector<int>& nums) {
 
         int n=nums.size();
-        if(n==0) return 0;
-        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-        // return solve(nums,0,-1,dp);    
-        for(int i=n-1;i>=0;i--){
-            for(int j=i-1;j>=-1;j--){
-                int np = dp[i+1][j+1];
-                int t =0;
-                if(j==-1 || nums[i]>nums[j]){
-                    t = 1+ dp[i+1][i+1];
+
+        //Tabulation Two states..
+        // if(n==0) return 0;
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        // // return solve(nums,0,-1,dp);    
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=i-1;j>=-1;j--){
+        //         int np = dp[i+1][j+1];
+        //         int t =0;
+        //         if(j==-1 || nums[i]>nums[j]){
+        //             t = 1+ dp[i+1][i+1];
+        //         }
+        //         dp[i][j+1]=max(t,np);
+        //     }
+        // }  
+
+        //One State Tabulation
+        vector<int> dp(n,1);
+        int maxi =1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                    maxi = max(maxi,dp[i]);
                 }
-                dp[i][j+1]=max(t,np);
             }
-        }  
-        return dp[0][0];  
+        }
+        return maxi;  
     }
 };
