0389f5bcc5f238167a8fa36104f8d15839e70ff7
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-11 21:59:43 +0530

    Time: 92 ms (6.68%) | Memory: 9.9 MB (95.67%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -12,52 +12,51 @@
 class Solution {
 public:
 
-    //brute force 
 
-    // void checkmini(TreeNode* root,int &mini,int parent){
-    //     if(root==NULL) return;
+    void checkmini(TreeNode* root,int &mini,int parent){
+        if(root==NULL) return;
         
-    //     checkmini(root->left,mini,parent);
-    //     if(root->val!=parent) mini = max(mini,abs(parent-(root->val)));
-    //     checkmini(root->right,mini,parent);
-    // }
-
-    // void io(TreeNode* root,int &mini){
-    //     if(root==NULL) return;
-
-    //     io(root->left,mini);
-    //     checkmini(root,mini,root->val);
-    //     io(root->right,mini);
-    // }
-    // int maxAncestorDiff(TreeNode* root) {
-    //     int mini = 0;
-    //     io(root,mini);  
-    //     return mini;      
-    // }
-
-    //optimal
-    pair<int,int> minForNodes(TreeNode* root,int &ans){
-        if(root->left==NULL && root->right==NULL) return {root->val,root->val};
-
-        int mini = INT_MAX,maxi = INT_MIN;
-        if(root->left){
-            pair<int,int> left = minForNodes(root->left,ans);
-            mini = min({left.first,mini,root->left->val});
-            maxi = max({left.second,maxi,root->left->val});
-        }
-        if(root->right){
-            pair<int,int> right = minForNodes(root->right,ans);
-            mini = min({right.first,mini,root->right->val});
-            maxi = max({right.second,maxi,root->right->val});
-        }
-
-        ans = max({ans,abs((root->val)-mini),abs((root->val)-maxi)});
-        return {mini,maxi};
+        checkmini(root->left,mini,parent);
+        if(root->val!=parent) mini = max(mini,abs(parent-(root->val)));
+        checkmini(root->right,mini,parent);
     }
 
+    void io(TreeNode* root,int &mini){
+        if(root==NULL) return;
+
+        io(root->left,mini);
+        checkmini(root,mini,root->val);
+        io(root->right,mini);
+    }
     int maxAncestorDiff(TreeNode* root) {
-        int anss = INT_MIN;
-        minForNodes(root,anss);
-        return anss;      
+        int mini = 0;
+        io(root,mini);  
+        return mini;      
     }
+
+    // //optimal
+    // pair<int,int> minForNodes(TreeNode* root,int &ans){
+    //     if(root->left==NULL && root->right==NULL) return {root->val,root->val};
+
+    //     int mini = INT_MAX,maxi = INT_MIN;
+    //     if(root->left){
+    //         pair<int,int> left = minForNodes(root->left,ans);
+    //         mini = min({left.first,mini,root->left->val});
+    //         maxi = max({left.second,maxi,root->left->val});
+    //     }
+    //     if(root->right){
+    //         pair<int,int> right = minForNodes(root->right,ans);
+    //         mini = min({right.first,mini,root->right->val});
+    //         maxi = max({right.second,maxi,root->right->val});
+    //     }
+
+    //     ans = max({ans,abs((root->val)-mini),abs((root->val)-maxi)});
+    //     return {mini,maxi};
+    // }
+
+    // int maxAncestorDiff(TreeNode* root) {
+    //     int anss = INT_MIN;
+    //     minForNodes(root,anss);
+    //     return anss;      
+    // }
 };
