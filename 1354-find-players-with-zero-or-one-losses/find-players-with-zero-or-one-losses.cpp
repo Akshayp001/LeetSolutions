86a74e92a53fb8827ed1cac617db2fc23914f920
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-15 11:27:08 +0530

    Time: 456 ms (78.14%) | Memory: 175 MB (49.58%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -5,16 +5,16 @@ public:
         unordered_map<int,int> loosers;
         vector<vector<int>> ans(2);
 
-        for(auto i:matches){
+        for(auto &i:matches){
             winners[i[0]]++;
             loosers[i[1]]++;
         }
 
-        for(auto i:winners)
+        for(auto &i:winners)
             if(loosers[i.first]==0)    
                 ans[0].push_back(i.first);
         
-        for(auto j:loosers) if(j.second==1) ans[1].push_back(j.first);
+        for(auto &j:loosers) if(j.second==1) ans[1].push_back(j.first);
 
         sort(ans[0].begin(),ans[0].end());
         sort(ans[1].begin(),ans[1].end());
