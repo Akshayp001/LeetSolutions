008e4e77e751cba6cf08575c5d4b6a237b03c3ed
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-15 11:37:13 +0530

    Time: 661 ms (24.37%) | Memory: 161.4 MB (92.57%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int,int> winners;
-        unordered_map<int,int> loosers;
+        unordered_map<int,int> track;
         vector<vector<int>> ans(2);
 
         for(auto &i:matches){
-            winners[i[0]]++;
-            loosers[i[1]]++;
+            if(track[i[0]]==0) track[i[0]]++;
+            if(track[i[1]]==0) track[i[1]]--;
+            
+            else if(track[i[1]]>0) track[i[1]]=-1;
+            else track[i[1]]--;  
         }
 
-        for(auto &i:winners)
-            if(loosers[i.first]==0)    
-                ans[0].push_back(i.first);
-        
-        for(auto &j:loosers) if(j.second==1) ans[1].push_back(j.first);
-
+        for(auto i:track){
+            cout<<i.first<<" "<<i.second<<endl;
+            if(i.second==-1) ans[1].push_back(i.first);
+            if(i.second>0) ans[0].push_back(i.first);
+        }
         sort(ans[0].begin(),ans[0].end());
         sort(ans[1].begin(),ans[1].end());
         return ans;        
