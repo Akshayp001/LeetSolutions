eb258485f492af7926354cc8540d334e35c4af90
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-23 16:37:25 +0530

    Time: 8 ms (49.83%) | Memory: 30.8 MB (5.77%) - LeetSync

--- 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
+++ 3214-maximize-area-of-square-hole-in-grid/maximize-area-of-square-hole-in-grid.cpp
@@ -2,26 +2,18 @@ class Solution {
 public:
 
     void maxConsecRange(vector<int> &arr,int &maxi){
-        int prev = arr[0];
-        int count=1;
+        int prev = arr[0],count=1;
         for(int i=1;i<arr.size();i++){
-            if(arr[i]==arr[i-1]+1){
-                count++;
-                maxi = max(maxi,count);
-            }else{
-                count =1;
-            }
+            if(arr[i]==arr[i-1]+1) count++, maxi = max(maxi,count);
+            else count =1;
             prev= arr[i];                        
         } 
     }
+
     int maximizeSquareHoleArea(int n, int m, vector<int>& h, vector<int>& v) {
-        sort(h.begin(),h.end());
-        sort(v.begin(),v.end());
-        // int hs=h[0],he=h[0],vs=v[0],ve=v[0];
+        sort(h.begin(),h.end()); sort(v.begin(),v.end());
         int hmaxi=1,vmaxi=1;
-        maxConsecRange(h,hmaxi);
-        maxConsecRange(v,vmaxi);
-        cout<<hmaxi<<" "<<vmaxi<<endl;
+        maxConsecRange(h,hmaxi); maxConsecRange(v,vmaxi);
         return pow(min(hmaxi,vmaxi)+1,2);             
     }
 };
