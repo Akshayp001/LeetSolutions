75f481a70bd75eb402d3eed2808a42c590e13f8b
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-01-25 00:14:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (18.28%) - LeetSync

--- 2850-construct-the-longest-new-string/construct-the-longest-new-string.cpp
+++ 2850-construct-the-longest-new-string/construct-the-longest-new-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int longestString(int x, int y, int z) {
-        return min(x,y)==max(x,y)?(min(x,y)*2+z)*2:(min(x,y)*2+z+1)*2;  
+        return (min(x,y)*2+z+(min(x,y)!=max(x,y)))*2;  
     }
 };
