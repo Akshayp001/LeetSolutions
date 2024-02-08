c583fafd265eea56742d3b26c689ad1cc9db6b7f
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-02-08 21:30:10 +0530

    Time: 273 ms (17.04%) | Memory: 12.7 MB (15.99%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> gen(){
         vector<int> arr;
         for(int i=1;i<=100;i++){
-            sqrs.push_back(i*i);
+            arr.push_back(i*i);
         }
         return arr;
     }
@@ -12,20 +12,18 @@ public:
     int solve(int n,vector<int> &dp){
         if(n==0) return 0;
         if(n<0) return 1e5;
-        
-        if(dp[n]!=-1) return dp[n];
-        int cap = lower_bound(sqrs.begin(),sqrs.end(),n)-sqrs.begin();
-        int mini = INT_MAX;
-        for(int i=cap;i>=0;i--){
-            if(sqrs[i]<=n){
-                mini = min(mini,1+solve(n-sqrs[i],dp));            
-            }
-        }
-        return dp[n]=mini;        
-    }
 
+        if(dp[n]!=-1) return dp[n];
+
+        int last = lower_bound(sqrs.begin(),sqrs.end(),n)-sqrs.begin();
+        int mini = INT_MAX;
+        for(int i=last;i>=0;i--){
+            mini = min(mini,1+solve(n-sqrs[i],dp));
+        }
+        return dp[n]=mini;
+    }
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);     
+        vector<int> dp(n+1,-1);        
+        return solve(n,dp);
     }
 };
