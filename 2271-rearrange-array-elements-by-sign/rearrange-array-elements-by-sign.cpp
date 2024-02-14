faa92da3431419459763551e1144545d96f5e241
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-02-14 10:31:35 +0530

    Time: 92 ms (99.65%) | Memory: 125.7 MB (66.79%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); cin.tie(NULL);
         int n = nums.size();
         vector<int> ans(n,0);
         int e=0,o=1;
