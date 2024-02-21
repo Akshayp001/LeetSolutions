36596229e8cceff5361f98de2dd8350ef5aa4bfc
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-02-21 09:30:51 +0530

    Time: 109 ms (5.16%) | Memory: 8.7 MB (55.90%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int powerOfTwo(int n){
+    int log2(int n){
         if(n==0) return -1; 
         if(n<=1) return 0;
         int power=0;
@@ -9,15 +9,12 @@ public:
     }
     
     int rangeBitwiseAnd(int left, int right) {
-        int t = powerOfTwo(left);
+        int t = log2(left);
         if (left == right) return left;
-        if (t < 0) return 0; 
-        if (left == 0) return 0; 
+        if (left==0 || t<0) return 0; 
         if (right >= pow(2,t+1)) return 0;
         long long result = left;
-        for (long long i=left+1;i<=right;++i) {
-            result &= i;
-        }
+        for (long long i=left+1;i<=right;++i) result &= i;
         return result;
     }
 };
