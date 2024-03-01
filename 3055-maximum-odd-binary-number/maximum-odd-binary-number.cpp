991ea96e01a21ad2800244da83deadb4c120ffbb
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-01 22:17:36 +0530

    Time: 6 ms (34.24%) | Memory: 11.1 MB (8.36%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
         sort(s.rbegin(),s.rend());
-        s= s.substr(1)+s[0];
-        return s;
+        return s.substr(1)+s[0];
     }
 };
