9caa2173083b93a3986448a28481bdc5f4b9e2ab
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-03 15:41:36 +0530

    Time: 8 ms (59.97%) | Memory: 10.6 MB (51.40%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,30 +9,30 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        //Floyd Cycle Detection
+        // Floyd Cycle Detection
 
         if(head==NULL||head->next==NULL) return false;
-        // ListNode* node = new ListNode();
-        // node->next = head;
-        // ListNode* ptr1=node->next,*ptr2=node->next;
-        // while(ptr1!=nullptr && ptr2!=nullptr){
-        //     ptr1 = ptr1->next;
-        //     ptr2 = ptr2->next;
-        //     if(ptr2!=nullptr) ptr2=ptr2->next;
-        //     if(ptr1==ptr2) return true;
-        // }
-        // return false;
-
-        unordered_map<ListNode*,int> m;
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        while(true){
-            m[head]++;
-            if(m[head]>1) return true;
-            if(head->next) head=head->next;
-            else break;
+        ListNode* node = new ListNode();
+        node->next = head;
+        ListNode* ptr1=node->next,*ptr2=node->next;
+        while(ptr1!=nullptr && ptr2!=nullptr){
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+            if(ptr2!=nullptr) ptr2=ptr2->next;
+            if(ptr1==ptr2) return true;
         }
-        head=dummy->next;
         return false;
+
+        // unordered_map<ListNode*,int> m;
+        // ListNode* dummy = new ListNode(0);
+        // dummy->next = head;
+        // while(true){
+        //     m[head]++;
+        //     if(m[head]>1) return true;
+        //     if(head->next) head=head->next;
+        //     else break;
+        // }
+        // head=dummy->next;
+        // return false;
     }
 };
