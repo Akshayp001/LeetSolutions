b04fc69f75cdf8e17863cd68b8dc9b0813520ba6
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-03 11:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (66.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -7,23 +7,18 @@ public:
         ListNode* first = dummy;
         ListNode* second = dummy;
         int temp = n;
-
-        while (temp--) {
-            first = first->next;
-        }
-
+        while (temp--) first = first->next;
+        
         while ((first->next) != nullptr) {
             first = first->next;
             second = second->next;
         }
-
         ListNode* newNode = second->next;
         second->next = newNode->next;
         delete newNode;
 
         ListNode* newHead = dummy->next;
         delete dummy;
-
         return newHead;
     }
 };
