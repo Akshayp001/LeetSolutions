9bd9c91f838fa4ff62ef5a104f7bdeaa8c39d05e
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-11 19:01:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (54.73%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int p = pow(2,n);
+        for(int i=0;i<p;i++){
+            vector<int> temp;
+            for(int j=0;j<n;j++){
+                if((i>>j)&1){
+                    temp.push_back(nums[j]);
+                }                
+            }
+            ans.push_back(temp);
+        }
+        return ans;   
+    }
+};
