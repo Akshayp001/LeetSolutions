896263c21c4e54da07252e2a37e2d13384bfba3b
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-11 22:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (61.64%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public: 
     int minBitFlips(int start, int goal) {
-        bitset<32> a(start);
-        bitset<32> b(goal);
-        bitset<32> c=a^b;
-        return c.count();
+        return bitset<32>(start^goal).count();
     }
 };
