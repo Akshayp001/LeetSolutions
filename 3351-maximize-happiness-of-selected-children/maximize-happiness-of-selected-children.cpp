9e341d7c110eb60892ea537b0ad5c3ae18c1dc22
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-13 06:37:42 +0530

    Time: 178 ms (29.52%) | Memory: 125.9 MB (5.01%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        priority_queue<long long> pq;
+        for(auto &i:happiness) pq.push(i);
+        long long ans=0,inc=0;
+        
+        while(!pq.empty()){
+            if(k<=0) break;
+            if(pq.top()-inc<=0) break;
+            ans+=pq.top()-inc; pq.pop();
+            inc++; k--;
+        }
+        return ans;
+    }
+};
