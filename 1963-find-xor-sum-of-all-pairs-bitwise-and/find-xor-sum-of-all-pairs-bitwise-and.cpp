83c180934698745752f62a228aaf91ee4d74f8ed
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-03-23 14:54:16 +0530

    Time: 112 ms (25.62%) | Memory: 96 MB (36.36%) - LeetSync

--- 1963-find-xor-sum-of-all-pairs-bitwise-and/find-xor-sum-of-all-pairs-bitwise-and.cpp
+++ 1963-find-xor-sum-of-all-pairs-bitwise-and/find-xor-sum-of-all-pairs-bitwise-and.cpp
@@ -3,10 +3,10 @@ public:
     int getXORSum(vector<int>& arr1, vector<int>& arr2) {
         int bx = 0;
         for(auto &i:arr2) bx^=i;
-        int ans = 0;
+        int ax = 0;
         for(auto &i:arr1){
-            ans = ans ^ (i&bx);
+            ax = ax ^ i;
         }
-        return ans;
+        return ax&bx;
     }
 };
