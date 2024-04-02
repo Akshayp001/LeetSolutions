6279db45fa09e21da5e889e8b65609051d0a5a7e
Akshay Patil <98218011+Akshayp001@users.noreply.github.com>
2024-04-02 10:04:19 +0530

    Time: 3 ms (83.52%) | Memory: 13 MB (28.04%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -3,10 +3,10 @@ public:
     vector<int> grayCode(int n) {
         vector<int> ans= {0,1};
         if(n==1) return ans;
-        long long t = 2;
+        // long long t = 2;
         for(int i=1;i<n;i++){
             int sz= (int)ans.size();
-            int ti = pow(t,i);
+            int ti = 1<<i;
             for(int i=sz-1;i>=0;i--){
                 ans.push_back(ti+ans[i]);
             }
